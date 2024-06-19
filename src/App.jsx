67b5a7a69b745b2a917da374d4698dc12c2a6c92
Christine <tinegulmatico@gmail.com>
2024-06-20 07:14:43 +0800

    added maintanance page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Footer from './components/Footer'
 import GenerateForm from './components/GenerateForm'
 import Header from './components/Header'
+import UnderConstruction from './components/UnderConstruction'
 
 function App() {
   return (
-    <div className=" bg-zinc-800 min-h-screen flex flex-col">
+    <div className=" bg-zinc-800 min-h-screen flex flex-col h-screen justify-between">
       <Header />
-      <GenerateForm />
+      <UnderConstruction/>
+      {/* <GenerateForm /> */}
       <Footer />
     </div>
   )
